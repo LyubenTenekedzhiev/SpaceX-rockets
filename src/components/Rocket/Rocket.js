e88ae106e147e86ce42608a5080f3fb9593ee5c1
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-04-28 22:00:29 +0300

    after rendering the rockets and making them move

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import rocket from "../../assets/rocket.png";
 import thrust from "../../assets/thrust.png";
+import rocketTop from "../../assets/rocket_top.png";
 import classes from "./Rocket.module.css";
 
-function Rocket() {
+const Rocket = React.memo(({ firstStageFuel, firstStageEngines, secondStageFuel, secondStageEngines }) => {
+  const [marginBottom, setMarginBottom] = useState(0);
+  const [pathsHeight, setPathsHeight] = useState((window.innerHeight - (window.innerHeight - 0.17 * window.innerHeight)).toFixed(0));
+  const [firstFuel, setFirstFuel] = useState((firstStageFuel / firstStageEngines).toFixed(0));
+  const [secondFuel, setSecondFuel] = useState((secondStageFuel / secondStageEngines).toFixed(0));
+  const [secondStageStarted, setSecondStageStarted] = useState(false);
+  const [secondStageFinished, setSecondStageFinished] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (Number(pathsHeight) >= Number((window.innerHeight - 0.2 * window.innerHeight).toFixed(0)) || Number(secondFuel) <= 0) {
+        setSecondStageFinished(true);
+        clearInterval(timer);
+        return;
+      }
+      if (firstFuel <= 0) {
+        setSecondStageStarted(true);
+        setSecondFuel(Number(secondFuel - 5));
+      } else {
+        setFirstFuel(Number(firstFuel) - 5);
+      }
+      setMarginBottom(marginBottom + 15);
+      setPathsHeight(Number(pathsHeight) + 15);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [marginBottom, pathsHeight, firstFuel, secondFuel]);
+
   return (
-    <div className={classes.Rocket}>
-      <img src={rocket} alt='rocket' />
-      <img src={thrust} alt='thrust' />
+    <div className={classes.RocketPath}>
+      {!secondStageStarted && !secondStageFinished ? (
+        <div className={classes.Rocket} style={{ marginBottom: marginBottom + "px" }}>
+          <img src={rocket} alt='rocket' />
+          <img src={thrust} alt='thrust' />
+        </div>
+      ) : secondStageStarted && !secondStageFinished ? (
+        <div className={classes.Rocket} style={{ marginBottom: marginBottom + "px" }}>
+          <img src={rocketTop} alt='rocket' />
+          <img src={thrust} alt='thrust' />
+        </div>
+      ) : null}
     </div>
   );
-}
+});
 
 export default Rocket;
--- src/components/RocketGrid/RocketGrid.js
+++ src/components/RocketGrid/RocketGrid.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Rocket from "./../Rocket/Rocket";
 import classes from "./RocketGrid.module.css";
 
 function RocketGrid() {
-  return (
-    <div className={classes.RocketGrid}>
-      <Rocket />
-      <Rocket />
-      <Rocket />
-      <Rocket />
-    </div>
-  );
+  const [rocketData, setRocketData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v2/rockets")
+      .then((req) => req.json())
+      .then((res) => {
+        setRocketData(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(rocketData);
+  const rockets = rocketData.map((rocket) => {
+    return (
+      <Rocket
+        key={rocket.id}
+        firstStageFuel={rocket.first_stage.fuel_amount_tons}
+        firstStageEngines={rocket.first_stage.engines}
+        secondStageFuel={rocket.second_stage.fuel_amount_tons}
+        secondStageEngines={rocket.second_stage.engines}
+      />
+    );
+  });
+  return <div className={classes.RocketGrid}>{rockets}</div>;
 }
 
-export default RocketGrid;
+export default React.memo(RocketGrid);
